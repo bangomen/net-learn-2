aba0437c1ea62d51ae8e9d8e619371183d94c696
exeproapps <bangtobek@gmail.com>
2024-04-30 12:13:45 +0700

    New Code For Aritmatika & Converter Values to Double

--- net-learn-2/net-learn-2/Program.cs
+++ net-learn-2/net-learn-2/Program.cs
@@ -10,10 +10,34 @@ namespace net_learn_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Masukan Tulisan kamu disini (Angka):");
+            Console.Write("Masukan Angka 1:");
             string x = Console.ReadLine();
 
-            Console.WriteLine("Anda Menulis (Angka): {0}",x);
+            Console.Write("Masukan Angka 2:");
+            string y = Console.ReadLine();
+
+            Console.Write("Hasil Penjumlahan adalah = {0}", x + y);
+            Console.WriteLine("");
+
+            Console.Write("Masukan Angka 1 lagi :");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukan Angka 2 lagi :");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Hasil Penjumlahan dengan Converter Int = {0}",a+b);
+            Console.WriteLine("");
+            Console.Write("Hasil Pengurangan dengan Converter Int = {0}", a - b);
+            Console.WriteLine("");
+            Console.Write("Hasil Perkalian dengan Converter Int = {0}",Convert.ToDouble(a) * Convert.ToDouble(b));
+            Console.WriteLine("");
+            Console.Write("Hasil Pembagian dengan Converter Int = {0}", Convert.ToDouble(a) / Convert.ToDouble(b));
+            Console.WriteLine("");
+            Console.Write("Hasil Modulus(Sisa Pembagian) dengan Converter Int = {0}", a % b);
+
+
+
+
             Console.ReadLine();
         }
     }
