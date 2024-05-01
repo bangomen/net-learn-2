6a79a41e90d6f6ba24c1bc5945fb6c127826555a
exeproapps <bangtobek@gmail.com>
2024-05-01 13:24:53 +0700

    New Code for Class & Objects

---
+++ net-learn-2/net-learn-2/Manusia.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_learn_2
+{
+    internal class Manusia
+    {
+        int umur;
+        string nama;
+        public void SayHi()
+        {
+            Console.WriteLine("Hello, Apa kabar");
+        }
+    }
+}
--- net-learn-2/net-learn-2/Program.cs
+++ net-learn-2/net-learn-2/Program.cs
@@ -6,34 +6,10 @@ namespace net_learn_2
     {
         static void Main(string[] args)
         {
-            string a = "Hari senin";
-            char[] b = { 'h', 'e', 'l', 'l', 'o' };
-
-            Console.WriteLine(a.Length);
-            Console.WriteLine(a.IndexOf('i'));
-            Console.WriteLine(a[3]);
-
-            a = a.Insert(0, "ini adalah ");
-            Console.WriteLine(a);
-
-            a = a.Replace("senin","selasa");
-
-            Console.WriteLine(a);
-
-            if (a.Contains("rabu"))
-            {
-                Console.WriteLine("Ditemukan text=Hari ");
-            }
-            else
-            {
-                Console.WriteLine("Tidak Ditemukan text=Hari");
-
-            }
-
-            a=a.Remove(2);
-
-            Console.WriteLine(a);
-            
+            Manusia andy = new Manusia();
+            Manusia dody = new Manusia();
+            andy.SayHi();
+            dody.SayHi(); ;
 
             Console.ReadLine();
         }
