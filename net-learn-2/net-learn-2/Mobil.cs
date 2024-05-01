5709d041ce09bfb5dcd4af0ee49aa33654d5aa2f
exeproapps <bangtobek@gmail.com>
2024-05-01 13:37:42 +0700

    New code for Class Example

---
+++ net-learn-2/net-learn-2/Mobil.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_learn_2
+{
+    internal class Mobil
+    {
+        public string warna;
+        string merk;
+        public int kecepatan;
+
+        public void Klakson()
+        {
+            Console.WriteLine("Beeeb... Beeeebbb");
+        }
+    }
+}
--- net-learn-2/net-learn-2/Program.cs
+++ net-learn-2/net-learn-2/Program.cs
@@ -6,10 +6,23 @@ namespace net_learn_2
     {
         static void Main(string[] args)
         {
-            Manusia andy = new Manusia();
-            Manusia dody = new Manusia();
-            andy.SayHi();
-            dody.SayHi(); ;
+            Mobil toyota= new Mobil();
+            Mobil Dayhatsu = new Mobil();
+
+            toyota.Klakson();
+            toyota.warna = "Hijau";
+            toyota.kecepatan = 200;
+
+            Dayhatsu.warna = "Putih";
+            Dayhatsu.kecepatan = 180;
+
+            //Dayhatsu.Klakson();
+
+            Console.WriteLine("Warna Mobil Toyota adalah: "+ toyota.warna);
+            Console.WriteLine("Warna Mobil Dayhatsu adalah: "+ Dayhatsu.warna);
+            Console.WriteLine("Kecepatan Toyota adalah: " + toyota.kecepatan);
+            Console.WriteLine("Kecepatan Dayhatsu adalah: " + Dayhatsu.kecepatan);
+
 
             Console.ReadLine();
         }
